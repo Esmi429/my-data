df0c4988e133a8e09bca634c15649636f55487af
Esmi429 <simex58@gmail.com>
2026-09-18 10:56:18 +0300

    begining

---
+++ strings_method.py
@@ -0,0 +1,29 @@
+my_name="techCamp kenya"
+print(my_name.capitalize())
+text1="My name IS keVin" 
+print(text1)
+text2=text1.lower()
+print(text2)
+text4="     i am a student   "
+text5=text4.strip()
+print(text5)
+print(len(text5))
+sentence1="     PyThon  ProgrammING"
+print(sentence1)
+print(len(sentence1))
+print(sentence1)
+sentence1="     PyThon  ProgrammING"
+sentence1=sentence1.strip()
+sentence1=sentence1.capitalize()
+print(sentence1)
+sentence2="SOFTWARE DEVELOPMENT   "
+sentence2=sentence2.strip()
+sentence2=sentence2.lower()
+print(sentence2)
+sentence6="Alex Kimani"
+sentence6=sentence6.replace("Alex Kimani","Alex Mwangi")
+print(sentence6)
+sentence7="Python programming"
+sentence7=sentence7.count('o')
+print(sentence7)
+sentence8="Alex":"Brian":"Mike":"Kevin"
